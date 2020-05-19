bde674b0f9dc39bc4f3eeb39763b6f63942c91ff
LucaBianco94 <luca3.bianco@mail.polimi.it>
2020-05-19 16:04:06 +0200

    fix

--- server.js
+++ server.js
@@ -19,19 +19,7 @@ app.get("/",function(req, res){
     res.render("index")
 })
 
-app.get('/db', async (req, res) => {
-    try {
-      const client = await pool.connect();
-      const result = await client.query('SELECT * FROM test_table');
-      const results = { 'results': (result) ? result.rows : null};
-      res.render('pages/db', results );
-      client.release();
-    } catch (err) {
-      console.error(err);
-      res.send("Error " + err);
-    }
-  })
 
 app.listen(port, function(){
-    console.log("App Running" + port)
+    console.log("App Running " + port + " "+ pool)
 })
