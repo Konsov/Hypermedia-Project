72f5f626b63d1a2b3299d6ae3bc777ee4eb06613
LucaBianco94 <luca3.bianco@mail.polimi.it>
2020-05-19 15:52:42 +0200

    db

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 var express = require('express')
 var app = express()
 
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
 //set port
 var port = process.env.PORT || 8080
 
@@ -10,6 +18,19 @@ app.use(express.static(__dirname + "/public"))
 app.get("/",function(req, res){
     res.render("index")
 })
+
+app.get('/db', async (req, res) => {
+    try {
+      const client = await pool.connect();
+      const result = await client.query('SELECT * FROM test_table');
+      const results = { 'results': (result) ? result.rows : null};
+      res.render('pages/db', results );
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
 
 app.listen(port, function(){
     console.log("App Running" + port)
