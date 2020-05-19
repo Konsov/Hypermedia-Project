3d6661d90188ccea8ddead7c86504dc9dd7060e0
Konsov <44020258+Konsov@users.noreply.github.com>
2020-05-19 11:46:34 +0200

    inizialized

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express')
 var app = express()
 
 //set port
-var port = process.env.port || 8080
+var port = process.env.PORT || 8080
 
 app.use(express.static(__dirname))
 
@@ -12,5 +12,5 @@ app.get("/",function(req, res){
 })
 
 app.listen(port, function(){
-    console.log("App Running")
+    console.log("App Running" + port)
 })
