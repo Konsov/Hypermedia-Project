56b91d6571963aa05a0528e571a056bc868eaad7
LucaBianco94 <luca3.bianco@mail.polimi.it>
2020-05-21 16:55:18 +0200

    prova server

--- server.js
+++ server.js
@@ -3,11 +3,15 @@ var app = express()
 
 const { Pool } = require('pg');
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
+    user: "gkhdthfcbqtvvx",
+    password: "65dfcca59ab75b6a0e957d537cf7fe3c2cb6f47ea41b25b982338a096c231549",
+    host: "ec2-54-246-85-151.eu-west-1.compute.amazonaws.com",
+    port: 5432,
+    database: "d6brmv5hu2coro"
 });
+
+pool.connect()
+.then(() => console.log("Connected successfuly"))
 
 //set port
 var port = process.env.PORT || 8080
